aad5107e23c6daa1913ccaae3295300a8e516f5c
Zi Wang <80916549+zwang4-code@users.noreply.github.com>
2022-11-07 12:30:35 -0800

    Adding remaining unit test for MoveValidator.cs

--- UnitTests/Model/Word/MoveValidatorTest.cs
+++ UnitTests/Model/Word/MoveValidatorTest.cs
@@ -7,10 +7,18 @@ namespace UnitTests
 {
     public class MoveValidatorTests
     {
+        // Set up a mock Board
+        private char[,] _mockBoard = new char[15, 15];
+
         [SetUp]
         public void Setup()
         {
-
+            // Place 5 pieces on the board
+            _mockBoard[7, 7] = 'G';
+            _mockBoard[8, 7] = 'O';
+            _mockBoard[9, 7] = 'O';
+            _mockBoard[10, 7] = 'D';
+            _mockBoard[10, 8] = 'O';
         }
 
         [Test]
@@ -136,6 +144,219 @@ namespace UnitTests
 
             // Act and Assert
             return MoveValidator.CorrectMove(mr, gs);
+        }
+
+        [Test]
+        public static void Validate_Return_Negative_1_GivenIncorrectMove()
+        {
+            // Arrange: Make CorrectMove() method return false by making the first move off center
+            GameState gs = new GameState();
+            MoveRecorder mr = new MoveRecorder();
+            mr.Record(6, 6);  // make first move NOT in center
+            char[,] emptyBoard = new char[15, 15];
+
+            // Act
+            int result = MoveValidator.Validate(gs, emptyBoard, mr);
+
+            // Assert
+            Assert.AreEqual(-1, result);
+        }
+
+        [Test]
+        public void Validate_Return_Negative_1_GivenUnchangedBoard()
+        {
+            // Arrange: Set up a GameState.BoardChar() using the mockBoard to simulate that user
+            // did not make any change to the board
+            GameState gs = new GameState();
+            gs.BoardChar = _mockBoard;
+            MoveRecorder mr = new MoveRecorder();  // does not matter here
+
+
+            // Act
+            int result = MoveValidator.Validate(gs, _mockBoard, mr);
+
+            // Assert
+            Assert.AreEqual(-1, result);
+        }
+         
+        [Test]
+        public void Validate_Return_Sum_Given_Vertical_ValidWord()
+        {
+            // Arrange
+            // Set up a GameState.BoardChar() with the mockBoard 
+            GameState gs = new GameState();
+            gs.BoardChar = _mockBoard;
+            gs.FirstMove = false;
+
+            // Set movement directions and locations
+            MoveRecorder mr = new MoveRecorder();
+            mr.Direction = "V";                     // simulate vertical movement
+            mr.Fixed = 8;                           // col where tiles are placed
+            mr.Index.Add(9); mr.Index.Add(11);      // row where tiles are placed
+
+            // Set up an updated mockBoard to simulate that user has formed VALID WORDS in VERTICAL direction
+            char[,] _updateMockBoard = new char[15, 15];
+            _updateMockBoard = (char[,])_mockBoard.Clone();
+            _updateMockBoard[9, 8] = 'H';
+            _updateMockBoard[11, 8] = 'T';
+
+            // Calculate expected score: [H + O + T] + [O + H] 
+            int expected = 4 + 1 + 1 + 1 + 4;
+
+            // Act
+            int actual = MoveValidator.Validate(gs, _updateMockBoard, mr);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void Validate_Return_Negative_1_Given_Vertical_InValidWord()
+        {
+            // Arrange
+            // Set up a GameState.BoardChar() with the mockBoard 
+            GameState gs = new GameState();
+            gs.BoardChar = _mockBoard;
+            gs.FirstMove = false;
+
+            // Set movement directions and locations
+            MoveRecorder mr = new MoveRecorder();
+            mr.Direction = "V";                 // simulate vertical movement
+            mr.Fixed = 8;                       // col where tiles are placed 
+            mr.Index.Add(9); mr.Index.Add(11);  // row where tiles are placed
+
+            // Set up an updated mockBoard to simulate that user has formed INAVLID WORDS
+            char[,] _updateMockBoard = new char[15, 15];
+            _updateMockBoard = (char[,])_mockBoard.Clone();
+            _updateMockBoard[9, 8] = 'X';
+            _updateMockBoard[11, 8] = 'Z';
+
+            // Act
+            int actual = MoveValidator.Validate(gs, _updateMockBoard, mr);
+
+            // Assert
+            Assert.AreEqual(-1, actual);
+        }
+
+        [Test]
+        public void Validate_Return_Sum_Given_Horizontal_ValidWord()
+        {
+            // Arrange
+            // Set up a GameState.BoardChar() with the mockBoard 
+            GameState gs = new GameState();
+            gs.BoardChar = _mockBoard;
+            gs.FirstMove = false;
+
+            // Set movement directions and locations
+            MoveRecorder mr = new MoveRecorder();
+            mr.Direction = "H";                     // simulate horizontal movement
+            mr.Fixed = 9;                           // row where tiles are placed
+            mr.Index.Add(8); mr.Index.Add(9);       // col where tiles are placed
+            mr.Index.Add(10); mr.Index.Add(11);
+
+            // Set up an updated mockBoard to simulate that user has formed VALID WORDS in HORIZONTAL direction
+            char[,] _updateMockBoard = new char[15, 15];
+            _updateMockBoard = (char[,])_mockBoard.Clone();
+            _updateMockBoard[9, 8] = 'T';
+            _updateMockBoard[9, 9] = 'T';
+            _updateMockBoard[9, 10] = 'E';
+            _updateMockBoard[9, 11] = 'R';
+
+            // Calculate expected score: [O + T + T * 3 + E + R] + [T + O] 
+            int expected = 1 + 1 + 3 + 1 + 1 + 1 + 1;
+
+            // Act
+            int actual = MoveValidator.Validate(gs, _updateMockBoard, mr);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void Validate_Return_Negative_1_Given_Horizontal_InValidWord()
+        {
+            // Arrange
+            // Set up a GameState.BoardChar() with the mockBoard 
+            GameState gs = new GameState();
+            gs.BoardChar = _mockBoard;
+            gs.FirstMove = false;
+
+            // Set movement directions and locations
+            MoveRecorder mr = new MoveRecorder();
+            mr.Direction = "H";                     // simulate horizontal movement
+            mr.Fixed = 9;                           // row where tiles are placed
+            mr.Index.Add(8); mr.Index.Add(9);       // col where tiles are placed
+
+            // Set up an updated mockBoard to simulate that user has formed INVALID WORDS
+            char[,] _updateMockBoard = new char[15, 15];
+            _updateMockBoard = (char[,])_mockBoard.Clone();
+            _updateMockBoard[9, 8] = 'X';
+            _updateMockBoard[9, 9] = 'Z';
+
+            // Act
+            int actual = MoveValidator.Validate(gs, _updateMockBoard, mr);
+
+            // Assert
+            Assert.AreEqual(-1, actual);
+        }
+
+        [Test]
+        public void Validate_Return_Sum_Given_SingleMove_No_Direction_ValidWord()
+        {
+            // Arrange
+            // Set up a GameState.BoardChar() with the mockBoard 
+            GameState gs = new GameState();
+            gs.BoardChar = _mockBoard;
+            gs.FirstMove = false;
+
+            // Set movement directions and locations
+            MoveRecorder mr = new MoveRecorder();
+            mr.Direction = "N";                     // simulate NO direction as user adds ONLY ONE TILE
+            Tuple<int, int> location = new Tuple<int, int>(9, 8);
+            mr.Moves.Add(location);                 // position where tile is placed
+
+
+            // Set up an updated mockBoard to simulate that user has formed VALID WORDS
+            char[,] _updateMockBoard = new char[15, 15];
+            _updateMockBoard = (char[,])_mockBoard.Clone();
+            _updateMockBoard[9, 8] = 'N';
+
+            // Calculate expected score: [N + O] + [O + N]
+            int expected = 1 + 1 + 1 + 1;
+
+            // Act
+            int actual = MoveValidator.Validate(gs, _updateMockBoard, mr);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void Validate_Return_Negative_1_Given_SingleMove_InValidWord()
+        {
+            // Arrange
+            // Set up a GameState.BoardChar() with the mockBoard 
+            GameState gs = new GameState();
+            gs.BoardChar = _mockBoard;
+            gs.FirstMove = false;
+
+            // Set movement directions and locations
+            MoveRecorder mr = new MoveRecorder();
+            mr.Direction = "N";                     // simulate NO direction as user adds ONLY ONE TILE
+            Tuple<int, int> location = new Tuple<int, int>(9, 8);
+            mr.Moves.Add(location);                 // position where tile is placed
+
+
+            // Set up an updated mockBoard to simulate that user has formed INVALID WORDS
+            char[,] _updateMockBoard = new char[15, 15];
+            _updateMockBoard = (char[,])_mockBoard.Clone();
+            _updateMockBoard[9, 8] = 'Z';
+
+            // Act
+            int actual = MoveValidator.Validate(gs, _updateMockBoard, mr);
+
+            // Assert
+            Assert.AreEqual(-1, actual);
         }
     }
 }
